a9c63fcce7fcd2c94b2a100e462af62c4ad092de
yanisf <foris86@mail.ru>
2016-05-08 14:16:08 +0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,9 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -85,15 +88,20 @@ describe User do
   before { @user.save }
   let(:found_user) { User.find_by(email: @user.email) }
 
-  describe "with valid password" do
-    it { should eq found_user.authenticate(@user.password) }
-  end
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
 
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-    it { should_not eq user_for_invalid_password }
-    specify { expect(user_for_invalid_password).to be_false }
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
   end
-end
 end
